a9408dbabfa710a9db9eef91085669d0f4294c00
Cameron Baron <twitchin86@gmail.com>
2016-05-30 23:02:41 +1000

    Initial upload, contains FMOD demo scene and some standard assets.

---
+++ FMOD_Plugin_Testing/Assets/Scripts/Shoot.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour
+{
+	[FMODUnity.EventRef]
+	public string shoot = "event:/Pickup_1";
+
+	public GameObject bulletPrefab;
+	public GameObject bulletSpawn;
+	public float bulletSpeed;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			FMODUnity.RuntimeManager.PlayOneShot(shoot);
+			GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.transform.position, Quaternion.identity) as GameObject;
+			bullet.GetComponent<Rigidbody>().AddForce(transform.forward * bulletSpeed, ForceMode.Impulse);
+		}
+	}
+
+}
---
+++ FMOD_Plugin_Testing/Assets/Scripts/WallReverb.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using FMOD.Studio;
+
+public class WallReverb : MonoBehaviour
+{
+	FMOD.Geometry f_geometry;
+	FMOD.System lowlevelsystem;
+
+	// Use this for initialization
+	void Start ()
+	{
+		lowlevelsystem = FMODUnity.RuntimeManager.LowlevelSystem;
+		uint version;
+		lowlevelsystem.getVersion(out version);
+		lowlevelsystem.createGeometry(1, 5, out f_geometry);
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
