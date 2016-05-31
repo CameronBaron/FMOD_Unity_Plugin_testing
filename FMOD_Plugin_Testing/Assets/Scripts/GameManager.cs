194d3b4c1688139d56a865e3b16d47f0005db503
Cameron Baron <cameron.baron@ad.aie.edu>
2016-05-31 17:34:10 +1000

    Started adding UI

--- FMOD_Plugin_Testing/Assets/Scripts/GameManager.cs
+++ FMOD_Plugin_Testing/Assets/Scripts/GameManager.cs
@@ -5,11 +5,13 @@ using UnityStandardAssets.Characters.FirstPerson;
 public class GameManager : MonoBehaviour
 {
 	public GameObject player;
+	public Canvas pauseCanvas;
+
 	private bool paused = false;
 	// Use this for initialization
 	void Start ()
 	{
-		
+		pauseCanvas.enabled = false;
 	}
 	
 	// Update is called once per frame
@@ -23,11 +25,13 @@ public class GameManager : MonoBehaviour
 				Time.timeScale = 0;
 				player.GetComponent<FirstPersonController>().enabled = false;
 				paused = true;
+				pauseCanvas.enabled = true;
 				return;
 			}
 			Time.timeScale = 1;
 			player.GetComponent<FirstPersonController>().enabled = true;
 			paused = false;
+			pauseCanvas.enabled = false;
 		}
 	}
 }
